3f193127b225f1378b91d6711f4886987b321493
Reverhaus <javierbailengomez@gmail.com>
2024-01-22 21:50:30 +0100

    Iteration#1

---
+++ iteracion#6.js
@@ -0,0 +1,20 @@
+console.log("Ejercicio 1.1:")
+for (let i = 0; i <= 9; i++) {
+    console.log(i);
+    }
+
+console.log("Ejercicio 1.2:")
+for (let i = 0; i <= 9 ; i++) {
+    if (i%2 === 0) {
+        console.log(i)
+    }
+    }
+
+console.log("Ejercicio 1.3:")
+for (let i = 0; i <= 10 ;i++) {
+    if (i < 10) {
+        console.log('Intentando dormir 🐑', i);
+    }
+    else {i === 10; console.log("Dormido",i)
+    }
+}
